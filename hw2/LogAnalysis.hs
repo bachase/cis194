96c9db04500db376449ce7348a4cf6002ba5a66f
Brad Chase <bachase@gmail.com>
2014-05-24 21:10:16 +0000

    HW2 done

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -18,3 +18,32 @@ parseMessage s = f $ words s
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
+
+messageTime :: LogMessage -> Int
+messageTime (LogMessage _ t _) = t
+messageTime (Unknown _)        = 0
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) inTree = inTree
+insert inMesg Leaf        = Node Leaf inMesg Leaf
+insert inMesg (Node treeL m treeR)
+    | (messageTime inMesg < messageTime m) = Node (insert inMesg treeL) m treeR
+    | otherwise                            = Node treeL m (insert inMesg treeR)
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node treeL m treeR) = (inOrder treeL) ++ [m] ++ (inOrder treeR)
+
+isBadErrMsg :: Int -> LogMessage -> Bool
+isBadErrMsg d (LogMessage (Error severity) _ _) = severity > d
+isBadErrMsg _ _= False
+
+getMsg :: LogMessage -> String
+getMsg (LogMessage _ _ s) = s
+getMsg (Unknown s) = s
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = map getMsg .filter (isBadErrMsg 50) . inOrder . build
