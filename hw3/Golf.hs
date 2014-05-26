efa4619de1b2522ef998c4910b90298a9f2fa4e7
Brad Chase <bachase@gmail.com>
2014-05-26 11:28:15 -0400

    HW 3 done

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 takeEvery :: Int -> [a] -> [a]
 takeEvery n = map snd . filter (\(x,y) -> (mod x n) == 0) . zip [1..]
 
@@ -14,5 +16,17 @@ skips xs = skips' xs (length xs)
 localMaxima :: [Integer] -> [Integer]
 localMaxima z = map (\(a,b,c) -> b) . filter (\(a,b,c) -> b > a && b > c ) $ zip3 z (drop 1 z) (drop 2 z)
 
+-- Problem 3
 
-histogram :: [Integer] -> String
+numOccurencesOf :: Int -> [Int] -> Int
+-- Count number occurences of of number in list
+numOccurencesOf n = length . filter (== n)
+
+strHistograms :: [Int] -> [String]
+strHistograms xs = [ (replicate (maxHist - c) ' ') ++ (replicate c '*') | c <- counts]
+    where counts = [ numOccurencesOf n xs | n <- [0..9] ]
+          maxHist = maximum counts
+
+histogram :: [Int] -> String
+histogram xs = z ++ "\n==========\n0123456789\n"
+    where z = intercalate "\n" . transpose $ strHistograms xs
